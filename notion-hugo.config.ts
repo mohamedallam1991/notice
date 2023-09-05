5409c84d504dee6f4b65598a41c415588b38dace
Mohamed Allam <meda3gm@gmail.com>
2023-09-05 11:08:11 +0100

    Update notion-hugo.config.ts
    adding the page

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -3,7 +3,7 @@ import { UserConfig } from "./src/config"
 const userConfig: UserConfig = {
     mount: {
         manual: false,
-        page_url: 'https://pcloud.notion.site/Notion-DoIt-04bcc51cfe4c49938229c35e4f0a6fb6',
+        page_url: 'https://mohamedallam.notion.site/Notion-DoIt-7b88087ae85d4ed78a08614f584467fc',
         pages: [
             // {
             //     page_id: '<page_id>',
